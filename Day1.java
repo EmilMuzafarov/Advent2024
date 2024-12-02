62f099876b13a65bcf593134b35e56d01724c85b
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2024-12-02 14:02:20 -0500

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Day1 {
+    static ArrayList<Integer> row1=new ArrayList<>();
+    static ArrayList<Integer> row2=new ArrayList<>();
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/input");
+        for (int i=0; i<fileData.size(); i++) {
+            String sample = fileData.get(i);
+            String[] splitSample = sample.split(" ");
+            row1.add(Integer.parseInt(splitSample[0]));
+            row2.add(Integer.parseInt(splitSample[1]));
+        }
+        System.out.println(findMin(-1, row1));
+    }
+    public static int findMin(int param, ArrayList<Integer> arr) {
+        int min=999999999;
+        if (param>-1) {
+            min=param;
+        }
+        for (int a=0; a<arr.size(); a++) {
+            if (arr.get(a)<min) {
+                min=arr.get(a);
+            }
+        }
+        return min;
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
