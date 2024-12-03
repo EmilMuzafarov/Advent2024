c651baad892e21107206aeed564ab19af4113894
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2024-12-03 14:05:59 -0500

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.util.ArrayList;
+
+public class Day3 {
+    public static void main(String[] args) {
+        String searchString = getFileData("src/input");
+        ArrayList<String> allMatches = new ArrayList<String>();
+        String regex = "mul\\([1-9][1-9][1-9],[1-9][1-9][1-9]\\)";
+        Matcher m = Pattern.compile(regex).matcher(searchString);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        System.out.println(allMatches);
+        int sum=0;
+        for (int i=0; i<allMatches.size(); i++) {
+            sum+=mul(allMatches.get(i));
+        }
+        System.out.println(sum);
+    }
+    public static int mul(String test) {
+        String[] splitSample = test.split(",");
+        String[] split1 = test.split("\\(");
+        String[] split2 = test.split("\\)");
+        int num1=Integer.parseInt(split1[1]);
+        int num2=Integer.parseInt(split2[1]);
+        int product=num1*num2;
+        System.out.println(product);
+        return product;
+    }
+    public static String getFileData(String fileName) {
+        String fileData = "";
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData+=line;
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
