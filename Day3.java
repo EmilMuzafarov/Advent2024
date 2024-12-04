602383216a08c7efe96ba7c9b398de709a65708e
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2024-12-04 14:07:26 -0500

    Update Day3.java 12/4

--- Day3.java
+++ Day3.java
@@ -9,7 +9,7 @@ public class Day3 {
     public static void main(String[] args) {
         String searchString = getFileData("src/input");
         ArrayList<String> allMatches = new ArrayList<String>();
-        String regex = "mul\\([1-9][1-9][1-9],[1-9][1-9][1-9]\\)";
+        String regex = "mul\\([1-9]{1-3},[1-9]{1-3}\\)";
         Matcher m = Pattern.compile(regex).matcher(searchString);
         while (m.find()) {
             allMatches.add(m.group());
@@ -23,12 +23,11 @@ public class Day3 {
     }
     public static int mul(String test) {
         String[] splitSample = test.split(",");
-        String[] split1 = test.split("\\(");
-        String[] split2 = test.split("\\)");
+        String[] split1 = splitSample[0].split("\\(");
+        String[] split2 = splitSample[1].split("\\)");
         int num1=Integer.parseInt(split1[1]);
-        int num2=Integer.parseInt(split2[1]);
+        int num2=Integer.parseInt(split2[0]);
         int product=num1*num2;
-        System.out.println(product);
         return product;
     }
     public static String getFileData(String fileName) {
