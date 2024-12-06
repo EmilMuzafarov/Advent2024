4c885c65e388ff9906200bbd0ddb592c2ac9baa4
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2024-12-06 14:07:03 -0500

    Day 1 solved 12/6

--- Day1.java
+++ Day1.java
@@ -5,40 +5,29 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Arrays;
 import static java.lang.Integer.parseInt;
+import java.util.Collections;
 
 public class Day1 {
-    static List<Integer> row1=new ArrayList();
+    static ArrayList<Integer> row1=new ArrayList();
     static ArrayList<Integer> row2=new ArrayList<>();
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/input");
         for (int i=0; i<fileData.size(); i++) {
             String sample = fileData.get(i);
-            String[] splitSample = sample.split(" ");
+            String[] splitSample = sample.split("   ");
             row1.add(Integer.parseInt(splitSample[0]));
             row2.add(Integer.parseInt(splitSample[1]));
         }
-        ArrayList<Integer> min1 = new ArrayList<>();
-        ArrayList<Integer> min2 = new ArrayList<>();
-        int sum1=0;
-        int small1=0;
-        for (int x=0; x<row1.size(); x++) {
-            min1.add(findMin(small1,row1.get(x)));
-        }
-        System.out.println(findMin(-1, row1));
-    }
-    public static int findMin(int param, ArrayList<Integer> arr) {
-        int min=999999999;
-        if (param>-1) {
-            min=param;
-        }
-        for (int a=0; a<arr.size(); a++) {
-            if (arr.get(a)<min) {
-                min=arr.get(a);
-            }
-        }
-        return min;
-    }
+        int sum=0;
+        Collections.sort(row1);
+        Collections.sort(row2);
 
+        for (int i=0; i<row1.size(); i++) {
+            int distance=Math.abs(row1.get(i)-row2.get(i));
+            sum+=distance;
+        }
+        System.out.println(sum);
+    }
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<String>();
         try {
