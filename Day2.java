7ac9a186bd3ea78a29143bdd7b6d1748540c47f5
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2024-12-11 14:00:34 -0500

    Day2.java solved

--- Day2.java
+++ Day2.java
@@ -1,27 +1,61 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 public class Day2 {
+
     public static void main(String[] args) {
+        ArrayList<Integer> numList=new ArrayList<Integer>();
         ArrayList<String> fileData = getFileData("src/input");
-        // build a 2D Array based on the length of each string and the size of the list
-        // e.g
-        // ABCD
-        // EFGH
-        // IJKL
-        // This would be a 3 row, 4 column 2D array
-        ArrayList<Integer> numlist=new ArrayList<Integer>();
-        for (int i=0; i<fileData.size(); i++) {
+        int safe = 0;
+        for (int i = 0; i < fileData.size(); i++) {
+            numList.clear();
             String[] splitSample = fileData.get(i).split(" ");
-            for (int j=0; j<splitSample.length; j++) {
-                numlist.add(Integer.parseInt(splitSample[j]));
+            for (int j = 0; j < splitSample.length; j++) {
+                numList.add(Integer.parseInt(splitSample[j]));
+                if (j == splitSample.length - 1) {
+                    safe += safe(numList);
+                }
             }
-            numlist.add(1111);
         }
-        System.out.println(numlist);
+        System.out.println(safe);
+    }
+    public static int safe(ArrayList<Integer> test) {
+        int outcome=0;
+        for (int i=0; i<test.size(); i++) {
+            if ((areIncreasing(test) || areDecreasing(test)) && differ(test)) {
+                outcome=1;
+            }
+        }
+        return outcome;
+    }
+    public static boolean areIncreasing(ArrayList<Integer> test) {
+        int prev=-1;
+        for (int i=0; i<test.size(); i++) {
+            if (test.get(i)<=prev) {
+                return false;
+            }
+            prev=test.get(i);
+        }
+        return true;
+    }
+    public static boolean areDecreasing(ArrayList<Integer> test) {
+        int prev=10000;
+        for (int i=0; i<test.size(); i++) {
+            if (test.get(i)>=prev) {
+                return false;
+            }
+            prev=test.get(i);
+        }
+        return true;
+    }
+    public static boolean differ(ArrayList<Integer> test) {
+        for (int i=0; i<test.size()-1; i++) {
+            if (!(Math.abs(test.get(i)-test.get(i+1))>=1 && Math.abs(test.get(i)-test.get(i+1))<=3)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
