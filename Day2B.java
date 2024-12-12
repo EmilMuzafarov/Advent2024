cf906b0f0bb2e8a0100ee0ff591ab992ad47b681
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2024-12-12 14:08:13 -0500

    Update Day2B.java

--- Day2B.java
+++ Day2B.java
@@ -3,12 +3,14 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Day2B {
-
+    static int tracker=0;
     public static void main(String[] args) {
         ArrayList<Integer> numList=new ArrayList<Integer>();
         ArrayList<String> fileData = getFileData("src/input");
         int safe = 0;
         for (int i = 0; i < fileData.size(); i++) {
+            tracker=0;
+            System.out.println();
             numList.clear();
             String[] splitSample = fileData.get(i).split(" ");
             for (int j = 0; j < splitSample.length; j++) {
@@ -22,20 +24,17 @@ public class Day2B {
     }
     public static int safe(ArrayList<Integer> test) {
         int outcome=0;
-        for (int i=0; i<test.size(); i++) {
-            if ((areIncreasing(test) || areDecreasing(test)) && differ(test)) {
-                outcome=1;
-            }
+        if ((areIncreasing(test) || areDecreasing(test)) && differ(test)) {
+            outcome=1;
         }
         return outcome;
     }
     public static boolean areIncreasing(ArrayList<Integer> test) {
         int prev=-1;
-        int tracker=0;
         for (int i=0; i<test.size(); i++) {
             if (test.get(i)<=prev) {
                 tracker++;
-                if (tracker==2) {
+                if (tracker>1) {
                     return false;
                 }
             }
@@ -45,11 +44,10 @@ public class Day2B {
     }
     public static boolean areDecreasing(ArrayList<Integer> test) {
         int prev=10000;
-        int tracker=0;
         for (int i=0; i<test.size(); i++) {
             if (test.get(i)>=prev) {
                 tracker++;
-                if (tracker==2) {
+                if (tracker>1) {
                     return false;
                 }
             }
@@ -58,11 +56,10 @@ public class Day2B {
         return true;
     }
     public static boolean differ(ArrayList<Integer> test) {
-        int tracker=0;
         for (int i=0; i<test.size()-1; i++) {
             if (!(Math.abs(test.get(i)-test.get(i+1))>=1 && Math.abs(test.get(i)-test.get(i+1))<=3)) {
                 tracker++;
-                if (tracker==2) {
+                if (tracker>1) {
                     return false;
                 }
             }
