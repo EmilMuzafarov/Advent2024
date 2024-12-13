4be0ae081fbaa9e69a8c24545ac150c64efb3a25
EmilMuzafarov <126209993+EmilMuzafarov@users.noreply.github.com>
2024-12-13 13:47:39 -0500

    Update Day2B.java

--- Day2B.java
+++ Day2B.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Day2B {
     static int tracker=0;
+    static int check1=0;
+    static int check2=0;
     public static void main(String[] args) {
         ArrayList<Integer> numList=new ArrayList<Integer>();
         ArrayList<String> fileData = getFileData("src/input");
         int safe = 0;
         for (int i = 0; i < fileData.size(); i++) {
             tracker=0;
-            System.out.println();
             numList.clear();
             String[] splitSample = fileData.get(i).split(" ");
             for (int j = 0; j < splitSample.length; j++) {
@@ -25,16 +26,19 @@ public class Day2B {
     public static int safe(ArrayList<Integer> test) {
         int outcome=0;
         if ((areIncreasing(test) || areDecreasing(test)) && differ(test)) {
-            outcome=1;
+            if (!(check1>1 && check2>1)) {
+                outcome=1;
+            }
         }
         return outcome;
     }
     public static boolean areIncreasing(ArrayList<Integer> test) {
         int prev=-1;
+        check1=0;
         for (int i=0; i<test.size(); i++) {
             if (test.get(i)<=prev) {
-                tracker++;
-                if (tracker>1) {
+                check1++;
+                if (check1>1) {
                     return false;
                 }
             }
@@ -44,10 +48,11 @@ public class Day2B {
     }
     public static boolean areDecreasing(ArrayList<Integer> test) {
         int prev=10000;
+        check2=0;
         for (int i=0; i<test.size(); i++) {
             if (test.get(i)>=prev) {
-                tracker++;
-                if (tracker>1) {
+                check2++;
+                if (check2>1) {
                     return false;
                 }
             }
@@ -58,8 +63,12 @@ public class Day2B {
     public static boolean differ(ArrayList<Integer> test) {
         for (int i=0; i<test.size()-1; i++) {
             if (!(Math.abs(test.get(i)-test.get(i+1))>=1 && Math.abs(test.get(i)-test.get(i+1))<=3)) {
-                tracker++;
                 if (tracker>1) {
+                    return false;
+                }
+                if (check1<=1 || check2<=1) {
+                    tracker++;
+                } else {
                     return false;
                 }
             }
